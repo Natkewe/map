fba274c41605eaa5b544f1933aa4945ddea68e4b
Natkewe <kew_natalie@yahoo.co.uk>
2023-01-09 19:40:22 +0000

    Update all code exmaples

--- map.js
+++ map.js
@@ -5,15 +5,35 @@
  */
 
 // Using a for loop
+let nums = [1, 2, 3, 4, 5];
+let results = [];
+for (let num of nums) {
+  results.push(num * 2);
+}
+console.log(results);
+
 
 
 // Using map()
+const multiByTwo = function (num) {
+  return num * 2;
+}
+
+const mapResults = nums.map(multiByTwo);
+console.log(mapResults);
 
 
+//
 // Simplified w/ map()
+const simplified = nums.map(function (num) { return num * 2 });
+console.log(simplified);
+
 
 
 // Simplfied w/ map() + arrow function
+const arrow = nums.map(num => num * 2);
+console.log(arrow);
+
 
 
 // With objects:
@@ -37,3 +57,12 @@ const students = [
     skill: 'CSS'
   },
 ];
+
+const studentsWithIds = students.map(student => [student.name, student.id]);
+console.log(studentsWithIds);
+//
+// you can use any word instead of student and maple
+const study = students.map(maple => [maple.skill, maple.profession]);
+console.log(study);
+
+
