f37dd9e82ad82bf6f96ab624574b3106ad55d65f
Natkewe <kew_natalie@yahoo.co.uk>
2023-01-09 20:01:19 +0000

    Update with a few more examples

--- map.js
+++ map.js
@@ -13,6 +13,10 @@ for (let num of nums) {
 console.log(results);
 
 
+/**
+* When learning about map method you were taught it creates 
+* a new array for each item using a function you provide.
+*/
 
 // Using map()
 const multiByTwo = function (num) {
@@ -64,5 +68,41 @@ console.log(studentsWithIds);
 // you can use any word instead of student and maple
 const study = students.map(maple => [maple.skill, maple.profession]);
 console.log(study);
+
+//
+//
+//example challenge
+
+let students = [
+  {
+      name: 'John',
+      subjects: ['maths', 'english', 'cad'],
+      teacher: {maths: 'Harry', english: 'Joan', cad: 'Paul'},
+      results: {maths: 90, english: 75, cad: 87},
+  },
+  {
+      name: 'Emily',
+      subjects: ['science', 'english', 'art'],
+      teacher: {science: 'Iris', english: 'Joan', art: 'Simon'},
+      results: {science: 93, english: 80, art: 95},
+  },
+  {
+      name: 'Adam',
+      subjects: ['science', 'maths', 'art'],
+      teacher: {science: 'Iris', maths: 'Harry', art: 'Simon'},
+      results: {science: 93, maths: 77, art: 95},
+  },
+  {
+      name: 'Fran',
+      subjects: ['science', 'english', 'art'],
+      teacher: {science: 'Iris', english: 'Joan', art: 'Simon'},
+      results: {science: 93, english: 87, art: 95},
+  }
+];
+
+let [john, ...rest] = students.map(student => [student.name, student.results]);
+console.log(john);
+console.log(rest);
+
 
 
